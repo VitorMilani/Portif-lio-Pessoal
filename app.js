e3764f0e41095c866cfa9fe5819c856c605db11a
VitorMilani <162359280+VitorMilani@users.noreply.github.com>
2024-05-03 15:40:08 -0300

    Portifólio Pessoal

---
+++ app.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.Nome', {duration: 1500 });
+
+sr.reveal('.projetos', {
+    rotate: {x: 0, y:80, z:0},
+    duration: 2000})
+    
+sr.reveal('.sobre', {
+    rotate: {x: 0, y:80, z:0},
+    duration: 2000})
+
+ 
